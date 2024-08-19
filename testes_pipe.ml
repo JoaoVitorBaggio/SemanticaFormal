2b4c6d054fac7753f9f196ca338e09226f4a8193
Mateus Kienzle <63136680+mateuskienzle@users.noreply.github.com>
2024-08-19 13:23:11 -0300

    Update testes_pipe.ml

--- testes_pipe.ml
+++ testes_pipe.ml
@@ -12,3 +12,14 @@ int_bse (Pipe (Num 2, Fn ("x", TyInt, Binop (Mult, Var "x", Num 2))))  (* retorn
 int_bse (Pipe (Num 2, Binop (Mult, Num 3, Num 2)))  (* retorno => erro de tipo - tipo função era esperado *)
 
 int_bse (Pipe (Bool false, Fn ("x", TyInt, Binop (Sum, Var "x", Num 1))))  (* retorno => erro de tipo - tipo argumento errado *)
+
+
+let teste_pipe= 
+  Let ("incr", TyFn (TyInt, TyInt), 
+       Fn ("x", TyInt, Binop (Sum, Var "x", Num 1)),
+       Let ("dobro", TyFn (TyInt, TyInt), 
+            Fn ("x", TyInt, Binop (Mult, Num 2, Var "x")),
+            Let ("triplo", TyFn (TyInt, TyInt), 
+                 Fn ("x", TyInt, Binop (Mult, Num 3, Var "x")),
+                 Pipe (Pipe (Pipe (Num 5, Var "dobro"), Var "triplo"), Var "incr"))))
+let _ = int_bse(teste_pipe)
